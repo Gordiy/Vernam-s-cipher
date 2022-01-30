c0279a45d1b8affe41d1aa11423be40f602db08a
hordii.rushynets <hordii.rushynets@globallogic.com>
2022-01-30 23:44:10 +0200

    Add LSFR implementation of polinom x^11+x^2+1

---
+++ LFSR/lfsr.py
@@ -0,0 +1,31 @@
+"""
+Linear feedback shift register. 
+"""
+
+
+class LFSR:
+    """
+    x^11+x^2+1
+    """
+    @staticmethod
+    def get_key():
+        s = []
+        register = [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+
+        for i in range(3255):
+            s.append(register[0] ^ register[2] ^ register[11])
+            first_byte = register[0]
+            register[0] = register[11] ^ register[10]
+
+            # register[11] = register[10]
+            # register[10] = register[9]
+
+            for j in range(len(register)-1, 1, -1):
+                register[j] = register[j-1]
+
+            register[1] = first_byte
+
+        return s
+
+
+LFSR.get_key()
